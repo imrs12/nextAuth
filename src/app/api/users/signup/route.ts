3cfa1488761842517f5090f03d5f966927c326d0
Rahul Shah <rahulpshah012@gmail.com>
2025-07-03 14:25:46 +0530

    signup endpoint created

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,48 @@
+import dbConnect from "@/dbConfig/dbConfig";
+import User from "@/model/userModel";
+import { NextRequest, NextResponse} from "next/server"
+import bcrypt from "bcryptjs";
+import { sendEmail } from "@/utils/mail";
+
+dbConnect();
+
+export async function POST(req: NextRequest){
+    try {
+        const reqBody = await req.json();
+        const {username, email, password} = reqBody;
+
+        //checking whether user already or not
+        const user = await User.findOne({email})
+        if( user ){
+            return NextResponse.json({error: "User Already Exists"}, { status: 400})
+        }
+        
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt);
+        const hashedPassword = bcrypt.hash(password,salt)
+
+        //creating new user
+        const newUser = await User.create({
+            username,
+            email,
+            password
+        })
+        console.log(newUser);
+
+        sendEmail({email, emailType: 'VERIFY', userId: newUser._id })
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            newUser
+        })
+        
+    } catch (error: unknown) {
+        return NextResponse.json({
+            error: error,
+        }, {
+            status: 500
+        })
+    }
+    
+}
