fadfa4b4c1d5dbdcbbd3d57b9be38411ee2ae112
Rahul Shah <rahulpshah012@gmail.com>
2025-07-04 20:18:59 +0530

    login endpoint completed

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,7 +1,50 @@
+import User from "@/model/userModel";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 import { NextRequest, NextResponse } from "next/server";
 
-export default function POST(req: NextRequest){
-        return NextResponse.json({
-            message: "Login Page"
-        })
+export default async function POST(req: NextRequest){
+        try {
+
+            const reqBody = await req.json();
+            const { email, password } = reqBody;
+
+            const user = await User.findOne({email});
+
+            if( !user ){
+                return NextResponse.json({message: "User does not exists"}, { status: 400})
+            }
+
+            const validPassword = await bcrypt.compare(password, user.password)
+
+            if( !validPassword ){
+                return NextResponse.json({message: "Password not matched "}, { status: 400 })
+            }
+            
+            const TokenData = {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+            const token = jwt.sign(TokenData, process.env.JWT, { expiresIn: "1d"});
+
+            const response = NextResponse.json({
+                token: token,
+                success: true,
+                message: "Login succesfully"
+            })
+
+            response.cookies.set("token", token,{
+                httpOnly: true
+            })
+
+            return response
+            
+        } catch (error: any) {
+           return NextResponse.json({
+            error: error.message
+           },{
+            status: 500
+           })
+        }
 }
